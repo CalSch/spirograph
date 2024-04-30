0f867267ed0d01297bbd817313301f462275c52a
Cal Schwick <35741152+CalSch@users.noreply.github.com>
2024-04-30 09:08:01 -0500

    added a trace mode

--- script.js
+++ script.js
@@ -28,6 +28,16 @@ for (let i=0;i<5;i++) {
 let sticksSpeed = 0.75 + 0.001;
 let sticksScale = 180;
 
+const TraceType = {
+    None: 0,
+    Line: 1,
+    Joint: 2,
+    End: 3,
+};
+
+let traceMode = TraceType.Line;
+let jointSize = 2;
+
 ctx.fillStyle = "black";
 ctx.fillRect(0,0,screenWidth,screenHeight);
 
@@ -62,7 +72,23 @@ function draw() {
         x += Math.cos(angle*Math.PI/180)*length;
         y += Math.sin(angle*Math.PI/180)*length;
 
-        ctx.lineTo(x,y);
+        switch (traceMode) {
+            case TraceType.Line:
+                ctx.lineTo(x,y);
+                break;
+            case TraceType.Joint:
+                ctx.moveTo(x,y);
+                ctx.arc(x,y,jointSize,0,Math.PI*2);
+                break;
+            case TraceType.End:
+                ctx.moveTo(x,y);
+                if (i==sticks.length-1)
+                    ctx.arc(x,y,jointSize,0,Math.PI*2);
+                break;
+            case TraceType.None:
+            default:
+                break;
+        }
 
         // Move the stick
         sticks[i] += speed;
